7bd52e0675845317bc370f6ffb4f48de36007aa9
Chenglong Zhang <chenglong.zhang@aylo.com>
2025-08-25 20:54:08 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
+import type { NextConfig } from "next";
+
 const repo = 'personal_page'; // <-- your GitHub repo name
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
+  output: 'export',
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
   trailingSlash: true,
