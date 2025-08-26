e5b7626a5c9d73a94d3db8692ec3573666e7198a
Chenglong Zhang <chenglong.zhang@aylo.com>
2025-08-25 21:29:58 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
   trailingSlash: true,
 };
 
