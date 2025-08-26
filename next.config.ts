3b954d8199c4cac3020f11792d1f6cf84a12f898
Chenglong Zhang <chenglong.zhang@aylo.com>
2025-08-25 22:07:03 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
-const repo = 'personal_page';
-
-const isProd = process.env.NODE_ENV === 'production';
+const repo = "personal_page";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? `/${repo}` : '',
+  output: "export",
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
   trailingSlash: true,
   images: {
     unoptimized: true, // required for static export
+  },
+  publicRuntimeConfig: {
+    basePath: isProd ? `/${repo}` : "",
   },
 };
 
